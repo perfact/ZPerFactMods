f2743307181b47c8c3fd3b4f1044a68eb832f3d0
Viktor Dick <viktor.dick@perfact.de>
2019-11-07 20:50:43 +0100

    Weaken dependency on python-perfact.
    In order to make these packages more independent, allowScriptModuls will
    no longer fail if python-perfact is not installed (although it will not
    do much, either).
    Also, ZServer_hideversion will no longer break if no ZServer is present,
    e.g. in Python3/Zope4 with WSGI.
    Finally, increase the version in order to allow perfact-dbutils-zope2 to
    reference a specific version from now on.

--- Products/ZPerFactMods/ZServer_hideversion.py
+++ Products/ZPerFactMods/ZServer_hideversion.py
@@ -2,5 +2,10 @@
 # Change behavior of ZServer so it does not tell the client which server version is running
 #
 
-import ZServer
-ZServer.zhttp_server.SERVER_IDENT = ''
+# On Zope4/Python3, there is no ZServer.
+# TODO: WSGI equivalent
+try:
+    import ZServer
+    ZServer.zhttp_server.SERVER_IDENT = ''
+except ImportError:
+    pass
