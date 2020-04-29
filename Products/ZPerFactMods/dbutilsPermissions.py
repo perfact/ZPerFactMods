447ad94d152ee46dff512a11d1c27eb374f9e37b
maxkreie <64472773+maxkreie@users.noreply.github.com>
2020-04-29 15:18:45 +0200

    T195381: only allow downloding excel table if user has role admin (#10)

--- Products/ZPerFactMods/dbutilsPermissions.py
+++ Products/ZPerFactMods/dbutilsPermissions.py
@@ -15,6 +15,8 @@ class DBUtilsPermissions(Implicit):
         return
     def delete(self):
         return
+    def export(self):
+        return
     def translate(self):
         return
     def edit_docs(self):
@@ -26,6 +28,7 @@ class DBUtilsPermissions(Implicit):
         ('zDB_Delete_Records', 'delete'),
         ('zDB_Update_Records', 'update'),
         ('zDB_Insert_Records', 'insert'),
+        ('zDB_Export_Records', 'export'),
         ('zI18N_Translate_Entries', 'translate'), # DEPRECATED
         ('zLayout_Edit_Docs', 'edit_docs')
     ]
