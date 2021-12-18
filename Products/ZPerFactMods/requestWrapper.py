c90e5cd258cde0aecfc1e62de161a8c27952122d
Viktor Dick <vd@perfact.de>
2021-12-18 11:33:27 +0100

    Add docstring

--- Products/ZPerFactMods/requestWrapper.py
+++ Products/ZPerFactMods/requestWrapper.py
@@ -10,6 +10,14 @@ import logging
 logger = logging.getLogger('Products.ZPerFactMods.requestWrapper')
 
 def call_hook(event, method):
+    """
+    Find a method named `method` in the current context and call it, unless the
+    published object has a title that contains NO_REQWRAP.
+    This is also called for a lot of internal methods like ZMI ressources,
+    which sometimes have to title property. Such errors as well as the
+    situation where the hook is not present are caught and ignored. Errors
+    while calling the hook, on the other hand, are caught and logged.
+    """
     hook = None
     try:
         title = event.request.PUBLISHED.title
