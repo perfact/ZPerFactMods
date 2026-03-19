c815366c4c7b9fcd30d118697d67b68706674abf
Viktor Dick <vd@perfact.de>
2026-03-19 14:44:15 +0100

    Release version 26.3.0

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import find_packages, setup
 
-version = '26.2.0'
+version = "26.3.0"
 
-setup(name='Products.ZPerFactMods',
-      version=version,
-      description="",
-      long_description="""\
+setup(
+    name="Products.ZPerFactMods",
+    version=version,
+    description="",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      package_data={
-          'Products.ZPerFactMods': ['www/*',]
-      },
-      include_package_data=True,
-      zip_safe=False,
-      namespace_packages=['Products'],
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="",
+    author="",
+    author_email="",
+    url="",
+    license="",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    package_data={
+        "Products.ZPerFactMods": [
+            "www/*",
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+    namespace_packages=["Products"],
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+)
